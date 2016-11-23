026f0d22f8aa75472c0a14e2484dcdd89a427f68
volaya <volayaf@gmail.com>
2016-11-23 12:15:16 +0100

    added 'copy to clipboard' button

--- reportingtool/reportdialog.py
+++ reportingtool/reportdialog.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 from qgis.PyQt import uic
 import sys
+from qgis.PyQt.QtGui import QApplication
 
 WIDGET, BASE = uic.loadUiType(
     os.path.join(os.path.dirname(__file__), 'reportdialog.ui'))
@@ -16,6 +17,16 @@ class ReportDialog(BASE, WIDGET):
         self.textBrowser.setText(info)
         self.fileLabel.setText("A copy of the report is available at <a href='#'>%s</a>" % reportFile)
         self.fileLabel.linkActivated.connect(self.openReportFile)
+        self.copyToClipboardButton.clicked.connect(self.copyToClipboard)
+
+        self.bar = QgsMessageBar()
+        self.bar.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
+        self.layout().insertWidget(1, self.bar)
+
+    def copyToClipboard(self):
+        clipboard = QApplication.clipboard()
+        clipboard.setText(self.textBrowser.text())
+        self.bar.pushMessage("", "Report text was copied to the clipboard", level=QgsMessageBar.SUCCESS, duration=5)
 
     def openReportFile(self):
         if sys.platform.startswith('darwin'):
