7e8da111e99a29e0224ccb11661eb0728034bfba
Alessandro Pasotti <apasotti@boundlessgeo.com>
2017-03-06 18:23:45 +0100

    Added command line tests

---
+++ reportingtool/tests/commandline_test.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+# Generate a report and checks for  the presence of
+# the main sections and second level sections
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+REPORT_SCRIPT_CMD="python ${SCRIPT_DIR}/../createreport.py"
+REPORT_PATH=$(${REPORT_SCRIPT_CMD}|sed -e 's/Report created at //')
+REPORT_TEXT=$(cat $REPORT_PATH| sed -e 's/\t/@@/g')
+
+
+OLDIFS=$IFS
+IFS=$'\n'
+
+
+declare -a SECTIONS=(
+"-QGIS plugins"
+"-Python information"
+"-QGIS providers"
+"-QGIS settings"
+"-Qt/PyQt information"
+"-QGIS information"
+"-System information" )
+
+declare -a SUB_SECTIONS=(
+"@@-Active C++ plugins"
+"@@-Available Python plugins"
+"@@-Active Python plugins"
+"@@-Python version"
+"@@-Python implementation"
+"@@-Exec prefix"
+"@@-Python binary path"
+"@@-Prefix"
+"@@-pip freeze"
+"@@-Module search paths"
+"@@-Available QGIS data provider plugins"
+"@@-Plugin repositories"
+"@@-Qt version"
+"@@-Qt image plugins"
+"@@-Qt library paths"
+"@@-Qt database plugins"
+"@@-PyQt version"
+"@@-SIP version"
+"@@-QGIS prefix path"
+"@@-QGIS library path"
+"@@-QGIS pkg data path"
+"@@-QGIS lib exec path"
+"@@-QGIS version"
+"@@-QGIS application state"
+"@@-CPU cores"
+"@@-Operating system"
+"@@-Installed RAM"
+"@@-Hostname"
+"@@-User name"
+"@@-Home directory"
+"@@-Processor" )
+
+
+function check_section {
+    echo -n "Checking for $1 ..."
+    EXP=$(echo "$1" | sed -e 's/-/\\-/')
+    echo $REPORT_TEXT | grep -q "$EXP"
+    if [ "1" == "$?" ]; then
+        echo "Test failed for $1!"
+        exit 1
+    fi
+    echo " OK"
+}
+
+for section in "${SECTIONS[@]}" ; do
+    check_section "$section"
+done
+
+for section in "${SUB_SECTIONS[@]}" ; do
+    check_section "$section"
+done
+
+IFS=$OLDIFS
